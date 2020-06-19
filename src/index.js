8f97d76d9e1d646b21e33560c15b5a3cb2be787b
emi190396 <emily.lorenz@oliverlorenz.com>
2020-06-19 14:29:11 +0200

    feat: initial version

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const base = require('@high-standards-js/base');
+const husky = require('@high-standards-js/husky')
+
+(async() => {
+    await base.checkAcceptedHighStandards();
+
+    let packageJsonOfConfig = base.getInitiatingProjectPackageJson();
+    
+    packageJsonOfConfig = await base.addDevDependency(packageJsonOfConfig, 'git-authors-cli');
+
+    packageJsonOfConfig = husky.addHookCommand(
+        packageJsonOfConfig, 
+        'pre-commit', 
+        'git-authors-cli'
+    )
+    
+    base.writeInitiatingProjectPackageJson(packageJsonOfConfig);
+})()
